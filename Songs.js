3b3cac1eba852154def5adadc2d29cfac0a8c48a
architagrawal <32601591+architagrawal@users.noreply.github.com>
2022-05-28 23:15:02 +0530

    Add files via upload

---
+++ Songs.js
@@ -0,0 +1,13 @@
+const Songs = [
+    {
+      id: 1,
+      favourite: false,
+      songName: "",
+      artist: "",
+      song:"",
+      imgSrc:
+        "",
+    },
+  ];
+  
+  export { Songs };
