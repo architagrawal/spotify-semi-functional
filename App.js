cd97e9d07853af50a36163244628a419c1baa123
architagrawal <32601591+architagrawal@users.noreply.github.com>
2022-05-28 23:05:06 +0530

    SemiFunctional spotify clone
    Only play songs, favorite song work. Need to mention song details in the Component/Songs.js file.
    Made using  : React, materialUI and firebase

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { LeftMenu } from './Components/LeftMenu';
+import { MainContainer } from './Components/MainContainer';
+import { RightMenu } from './Components/RightMenu';
+
+function App() {
+  return(
+  <div className="App">
+    <div className=""></div>
+    <LeftMenu />
+    <MainContainer />
+
+    
+    <div className="background"></div>
+
+  </div>
+  );
+}
+
+export default App;
